105c94514fe507ca337fff0e0e74dba12fdc43c9
Ron Gatenio <ron.gatenio@gmail.com>
2022-10-10 22:04:11 +0300

    removed the traversal nodes - there is a logical bug with them

--- pyprd/src/hbg.py
+++ pyprd/src/hbg.py
@@ -11,8 +11,6 @@ DEFAULT_CACHELINE_SIZE = 64
 class EdgeType(Enum):
     INTER_THREAD                = 'inter-thread'
     INTRA_THREAD                = 'intra-thread'
-    TRAVERSAL_POSTORDER         = 'traversal-postorder'
-    TRAVERSAL_REVERSE_POSTORDER = 'traversal-reverse-postorder'
 
 
 NodeLocation = namedtuple('NodeLocation', ['tid', 'tindex'])
@@ -32,50 +30,15 @@ class HBG:
         self._nodes_location = nodes_location
         self._cacheline_size = cacheline_size
 
-        self._original_graph            = nx.subgraph_view(self._graph, filter_edge=lambda u, v: self._graph[u][v]['type'] in (EdgeType.INTER_THREAD, EdgeType.INTRA_THREAD))
         self._inter_graph               = nx.subgraph_view(self._graph, filter_edge=lambda u, v: self._graph[u][v]['type'] == EdgeType.INTER_THREAD)
         self._intra_graph               = nx.subgraph_view(self._graph, filter_edge=lambda u, v: self._graph[u][v]['type'] == EdgeType.INTRA_THREAD)
-        
-        self._add_traversal_postorder_edges()
-        self._add_traversal_reverse_postorder_edges()
-
-        self._postorder_graph           = nx.subgraph_view(self._graph, filter_edge=lambda u, v: self._graph[u][v]['type'] != EdgeType.TRAVERSAL_REVERSE_POSTORDER)
-        self._reverse_postorder_graph   = nx.subgraph_view(self._graph, filter_edge=lambda u, v: self._graph[u][v]['type'] != EdgeType.TRAVERSAL_POSTORDER)
 
         self._vars: Set[Tuple[int, int]] = set()
         self._cache_lines: Dict[Tuple[int, int], Set[Tuple[int, int]]] = {}
 
         self._find_vars()
 
-        nx.freeze(self._graph)
-
-        cc = nx.find_cycle(self._postorder_graph)
-        z = '\n'.join(map(str, ((n1.itype.name, self._nodes_location[n1], n2.itype.name, self._nodes_location[n2], self._graph[n1][n2]['type'].name) for n1, n2 in cc)))
-        print(z)
-        # return
-        import ipdb; ipdb.set_trace()
-        # assert nx.is_directed_acyclic_graph(self._graph), 'HBG given graph is not a DAG'
-        assert nx.is_directed_acyclic_graph(self._inter_graph), 'HBG inter graph is not a DAG'
-        assert nx.is_directed_acyclic_graph(self._intra_graph), 'HBG intra graph is not a DAG'
-        assert nx.is_directed_acyclic_graph(self._original_graph), 'HBG original graph is not a DAG'
-        assert nx.is_directed_acyclic_graph(self._postorder_graph), 'HBG postorder graph is not a DAG'
-        assert nx.is_directed_acyclic_graph(self._reverse_postorder_graph), 'HBG reversed postorder graph is not a DAG'
-
-    def _add_traversal_postorder_edges(self):
-        """Adds edges that cause a postorder to visit an epoch node before all intra-parents of its inter-children"""
-        for epoch_node in self.epoch_nodes:
-            for inter_child in self.get_inter_children(epoch_node):
-                intra_parent = self.get_intra_parent(inter_child)
-                if intra_parent:
-                    self._graph.add_edge(intra_parent, epoch_node, type=EdgeType.TRAVERSAL_POSTORDER)
-
-    def _add_traversal_reverse_postorder_edges(self):
-        """Adds edges that cause a reversed postorder to visit an epoch node before all intra-children of its inter-parents"""
-        for epoch_node in self.epoch_nodes:
-            for inter_parent in self.get_inter_parents(epoch_node):
-                intra_child = self.get_intra_child(inter_parent)
-                if intra_child:
-                    self._graph.add_edge(epoch_node, intra_child, type=EdgeType.TRAVERSAL_REVERSE_POSTORDER)
+        assert nx.is_directed_acyclic_graph(self._graph), 'HBG graph is not a DAG'
 
     def _find_vars(self):
         mask = ((1 << 64) - 1) * self._cacheline_size
@@ -179,10 +142,10 @@ class HBG:
         return self.get_node_location(n1).tindex < self.get_node_location(n2).tindex
 
     def postorder(self) -> Generator[nodes.AbstractNode, None, None]:
-        return nx.dfs_postorder_nodes(self._postorder_graph)
+        return nx.dfs_postorder_nodes(self._graph)
 
     def reverse_postorder(self) -> Generator[nodes.AbstractNode, None, None]:
-        return nx.topological_sort(self._reverse_postorder_graph)
+        return nx.topological_sort(self._graph)
 
 
 class HBGBuilder:
