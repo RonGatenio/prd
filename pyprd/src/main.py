d1a8f781026c8829c54419c4d93af1040ff03c49
Ron Gatenio <ron.gatenio@gmail.com>
2022-10-10 23:18:06 +0300

    more tests in main

--- pyprd/src/main.py
+++ pyprd/src/main.py
@@ -1,11 +1,11 @@
-from genericpath import isfile
 from typing import Any, Dict, Set, Tuple
 from hbg_trace_parser import TraceParser
 from pdg import generate_mock_pdg
-from hbg import HBG, HBGBuilder
-from collections import namedtuple
+from hbg import HBG
+import time
 import networkx as nx
 import nodes
+import prd
 
 
 def analyze_cycle(g: HBG):
@@ -69,7 +69,7 @@ def merge(d1: Dict[Any, Set[Any]], d2: Dict[Any, Set[Any]]):
         d1.setdefault(k, set()).update(v)
 
 
-def compare(hbg: HBG, d1: Dict[ReadNode, Dict[Var, Set[WriteNode]]], d2: Dict[ReadNode, Dict[Var, Set[WriteNode]]], delta_d2: Dict[ReadNode, Dict[Var, Set[WriteNode]]]):
+def compare(hbg: HBG, d1: Dict[ReadNode, Dict[Var, Set[WriteNode]]], d2: Dict[ReadNode, Dict[Var, Set[WriteNode]]], delta_d2: Dict[ReadNode, Dict[Var, Set[WriteNode]]]=None):
     for r in hbg.read_nodes:
         dd1 = d1.get(r, {})
         dd2 = d2.get(r, {})
@@ -105,7 +105,22 @@ def build_ha_groups(hbg: HBG, delta_ha_groups: Dict[ReadNode, Dict[Var, Set[Writ
             ha_groups[n] = mycopy(prev)
             
     return ha_groups
+
+
+def build_ha_groups_per_thread(hbg: HBG, delta_ha_groups: Dict[ReadNode, Dict[Var, Set[WriteNode]]]):
+    ha_groups: Dict[int, Dict[Var, Set[WriteNode]]] = {}
+    
+    for tid in hbg.tids:
+        thread_nodes = hbg.get_thread_nodes(tid)
+        ha_groups[tid] = {}
         
+        for n in reversed(thread_nodes):
+            if n.itype != 'READ':
+                continue
+            merge(ha_groups[tid], delta_ha_groups.get(n, {}))
+            
+    return ha_groups
+
 
 def main():
     trace = TraceParser.from_file(r'H:\Projects\LLVM\llvm-project\py_persistency_race_detector\tests\traces\real\test_recipe.txt')
@@ -130,167 +145,68 @@ def main():
     # assert nx.is_directed_acyclic_graph(hbg._intra_graph)
     # import ipdb; ipdb.set_trace()
     
-    import time
-    import intervaltree
     
-    # s = time.time()
-    # hbg = trace.to_hbg(filter=False)
-    # print(f'hbg {time.time() - s} sec')
     
-    # for t in nodes.NodeType:
-    #     print(f'{t.name:6} {len(hbg.get_nodes_by_type(t))}')
-        
-    # t = intervaltree.IntervalTree()
-    # for n in hbg.read_write_nodes:
-    #     t.addi(*n.interval)
-        
-    # t.merge_equals()
+    # HBG
+    s = time.time()
+    hbg = trace.to_hbg(filter=True, max_lines=10000)
+    print(f'hbg {time.time() - s} sec')
     
-    # print({(i.begin, i.end) for i in t.all_intervals})
-    # import ipdb; ipdb.set_trace()
-    # import ipdb; ipdb.set_trace()
-    
-    hbg, pdg = None, None
-    
-    # import os
-    # if os.path.isfile('data.bin'):
-    #     with open('data.bin', 'rb') as f:
-    #         from pickle import Unpickler
-    #         upp = Unpickler(f)
-    #         hbg, pdg = upp.load()
-    
-    if not hbg:
-        s = time.time()
-        hbg = trace.to_hbg(filter=True, max_lines=10000)
-        print(f'hbg {time.time() - s} sec')
-    
-        
-        for t in nodes.NodeType:
-            print(f'{t.name:6} {len(hbg.get_nodes_by_type(t))}')
+    for t in nodes.NodeType:
+        print(f'{t.name:6} {len(hbg.get_nodes_by_type(t))}')
             
-    # analyze_cycle(hbg)
-    # # print('HBG is DAG' if nx.is_directed_acyclic_graph(hbg) else 'HBG is not DAG ):')
-    # analyze_vars(hbg)
-    # return
+    # PDG
+    s = time.time()
+    pdg = generate_mock_pdg(hbg)
+    print(f'pdg {time.time() - s} sec')
     
+    print(f'nodes count {len(hbg.read_nodes)}')
+    total = len(hbg.read_nodes)
     
-    # t = intervaltree.IntervalTree()
-    # for n in hbg.read_write_nodes:
-    #     t.addi(*n.interval)
-        
-    # t.merge_equals()
-    
-    # intervals = {(i.begin, i.end) for i in t.all_intervals}
-    # interval_sizes = {(i.end - i.begin) for i in t.all_intervals}
-    # print(interval_sizes)
-    # print(len(intervals))
-    # import ipdb; ipdb.set_trace()
-    
-    if not pdg:
-        s = time.time()
-        pdg = generate_mock_pdg(hbg)
-        print(f'pdg {time.time() - s} sec')
-        
-        print(f'nodes count {len(hbg.read_nodes)}')
-        total = len(hbg.read_nodes)
-    
-    import pickle
-    
-    # if not os.path.isfile('data.bin'):
-    #     with open('data.bin', 'wb') as f:
-    #         pp = pickle.Pickler(f)
-    #         pp.dump((hbg, pdg))
-    
-    
-    import prd
+    # PRD    
     p = prd.PersistencyRaceDetector(hbg, pdg)
     
+    ## op1
     s = time.time()
-    op1 = p.build_delta_ha_groups_opt1()
+    dop1 = p.build_delta_ha_groups_opt1()
     print(f'build_delta_ha_groups_opt1 {time.time() - s} sec')
     
     s = time.time()
-    ha1 = build_ha_groups(hbg, op1)
-    print(f'build_ha_groups {time.time() - s} sec')
-    
-    # s = time.time()
-    # op3 = p.build_delta_ha_groups_opt3()
-    # print(f'build_delta_ha_groups_opt3 {time.time() - s} sec')
+    op1 = build_ha_groups(hbg, dop1)
+    print(f'build_ha_groups_opt1 {time.time() - s} sec')
     
     s = time.time()
-    op4 = p.build_delta_ha_groups_opt4()
-    print(f'build_delta_ha_groups_opt4 {time.time() - s} sec')
+    top1 = build_ha_groups_per_thread(hbg, dop1)
+    print(f'build_ha_groups_per_thread {time.time() - s} sec')
     
-    print(compare(hbg, op4, ha1, op1))
+    for tid in hbg.tids:
+        for n in hbg.get_thread_nodes(tid):
+            if n.itype == 'READ':
+                break
+        if not compare(hbg, top1[tid], op1[n]):
+            print('NOT EQUAL')
+            
+    ## op2
+    s = time.time()
+    dop2 = p.build_delta_ha_groups_opt2()
+    print(f'build_delta_ha_groups_opt2 {time.time() - s} sec')
     
-    import ipdb; ipdb.set_trace()
+    s = time.time()
+    op2 = build_ha_groups(hbg, dop2)
+    print(f'build_ha_groups_opt2 {time.time() - s} sec')
     
-    # s = time.time()
-    # happens_after_sets = {}
-    # next = 0
-    # for i, n in enumerate(hbg.read_nodes):
-    #     if i / total > next:
-    #         next = (i / total) + 0.001
-    #         print(f'completed {i / total * 100}%')
-    #         print(f'ha0 so far {time.time() - s} sec')
-    #     happens_after_sets[n] = nx.descendants(hbg, n)
-    # print(f'ha0 {time.time() - s} sec')
+    ## op3
+    s = time.time()
+    op3 = p.build_ha_groups_opt3()
+    print(f'build_ha_groups_opt3 {time.time() - s} sec')
     
-    # s = time.time()
-    # hbg.add_edges_from(((n, d) for n in hbg.read_nodes for d in nx.descendants(hbg, n)))
-    # print(f'ha3 {time.time() - s} sec')
+    s = time.time()
+    op4 = p.build_ha_groups_opt4()
+    print(f'build_ha_groups_opt4 {time.time() - s} sec')
     
-    # next = 0
-    # wn = list(hbg.write_nodes)[-1]
-    # s = time.time()
-    # for i, n in enumerate(hbg.read_nodes):
-    #     wn in nx.descendants(hbg, n)
-    #     if i / total > next:
-    #         next = (i / total) + 0.0001
-    #         print(f'completed {i / total * 100}%')
-    #         print(f'ha4 so far {time.time() - s} sec')
-    # # hbg.add_edges_from(((n, d) for n in hbg.read_nodes for d in nx.descendants(hbg, n)))
-    # print(f'ha4 {time.time() - s} sec')
-    
-    # s = time.time()
-    # happens_after_sets = {}
-    # next = 0
-    # for i, n in enumerate(hbg):
-    #     if i / total > next:
-    #         next = (i / total) + 0.000001
-    #         print(f'completed {i / total * 100}%')
-    #         print(f'ha1 so far {time.time() - s} sec')
-    #     happens_after_sets[n] = nx.bfs_tree(hbg, n).nodes
-    # print(f'ha1 {time.time() - s} sec')
-    
-    # s = time.time()
-    # happens_after_sets2 = {}
-    # next = 0
-    # print(f'cc count {len(list(nx.connected_components(hbg.to_undirected())))}')
-    # for cc in nx.connected_components(hbg.to_undirected()):
-    #     collect = set()
-    #     for i, n in enumerate(nx.dfs_postorder_nodes(hbg)):
-    #         if i / total > next:
-    #             next = (i / total) + 0.01
-    #             print(f'completed {i / total * 100}%')
-    #             print(f'ha2 so far {time.time() - s} sec')
-    #         happens_after_sets2[n] = collect.copy()
-    #         collect.add(n)
-    # print(f'ha2 {time.time() - s} sec')
-    
-    # s = time.time()
-    # # print(f'cc count {len(list(nx.connected_components(hbg.to_undirected())))}')
-    # for cc in nx.connected_components(hbg.to_undirected()):
-    #     collect = set()
-    #     next = 0
-    #     for i, n in enumerate(nx.dfs_postorder_nodes(hbg)):
-    #         if i / total > next:
-    #             next = (i / total) + 0.01
-    #             print(f'completed {i / total * 100}%')
-    #             print(f'ha5 so far {time.time() - s} sec')
-    #         hbg.add_edges_from(((n, d) for d in collect))
-    #         collect.add(n)
-    # print(f'ha5 {time.time() - s} sec')
+    print(compare(hbg, op1, op2))
+    print(compare(hbg, op1, op3))
+    print(compare(hbg, op1, op4))
     
     import ipdb; ipdb.set_trace()
 
