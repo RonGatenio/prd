93b617369f778939d76b5671b0fd18d715d91771
Ron Gatenio <ron.gatenio@gmail.com>
2022-10-10 23:28:16 +0300

    formatting

--- pyprd/src/prd.py
+++ pyprd/src/prd.py
@@ -41,38 +41,38 @@ import intervaltree
 #     def __init__(self):
 #         self._itree = intervaltree.IntervalTree()
 #         self._vars = {}
-        
+
 #     # def __get__(self, var):
 #     #     # self._itree.
 #     #     pass
-    
+
 #     # def __set__(self, var, node):
-    
+
 #     @property
 #     def vars(self):
 #         return self._vars.keys()
-    
+
 #     def range(self, var: Var):
 #         for i in self._itree.envelop(*var):
 #             yield (i.begin, i.end)
-    
+
 #     def set(self, var: Var, data: Any):
 #         self._vars[var] = data
 #         self._itree.addi(*var)
-        
+
 #     def get(self, var: Var, default=None):
 #         return self._vars.get(var, default)
-        
+
 #     def set_all(self, var: Var, data: Any):
 #         for v in self.range(var):
 #             self._vars[v] = data
-    
+
 #     # def add(self, var: Var, data: Any):
 #     #     if var not in self._vars:
 #     #         self._vars[var] = intervaltree.Interval(*var, data=set())
 #     #         self._itree.add(self._vars[var])
 #     #     self._vars[var].data.add(data)
-        
+
 #     # def get(self, var: Var):
 #     #     a = set()
 #     #     for i in self._itree[var[0]:var[1]]:
@@ -96,7 +96,7 @@ class PersistencyRaceDetector:
         self._delta_flushed_read_nodes: Dict[nodes.InstructionNode, Dict[Var, Set[nodes.InstructionNode]]] = {}
         self._delta_flushed_write_nodes: Dict[nodes.InstructionNode, Dict[Var, Set[nodes.InstructionNode]]] = {}
         self._thread_contexts: Dict[ThreadId, ThreadContext] = {}
-        
+
     def build_delta_ha_groups_opt1(self):
         ReadNode = nodes.InstructionNode
         WriteNode = nodes.InstructionNode
@@ -104,37 +104,37 @@ class PersistencyRaceDetector:
         last_read_nodes_storage: Dict[nodes.EpochNode, NodeContextType] = {}
         last_read_nodes_current: Dict[ThreadId, NodeContextType] = {}
         delta_ha_groups: Dict[ReadNode, Dict[Var, Set[WriteNode]]] = {}
-        
+
         for n in self._hbg.reverse_postorder():
             if n.itype == nodes.NodeType.READ:
                 last_read_nodes_current.setdefault(n.tid, {})[n.tid] = n
-                
+
             elif n.itype == nodes.NodeType.WRITE:
                 n: nodes.InstructionNode
-                
+
                 for tid in self._hbg.tids:
                     last_read_node = last_read_nodes_current.setdefault(n.tid, {}).get(tid)
                     if last_read_node:
                         delta_ha_groups.setdefault(last_read_node, {}).setdefault(n.interval, set()).add(n)
-                        
+
             elif n.itype == nodes.NodeType.EPOCH:
                 n: nodes.EpochNode
-                
+
                 for parent in self._hbg.get_inter_parents(n):
                     assert isinstance(parent, nodes.EpochNode)
-                    
+
                     parent: nodes.EpochNode
-                    
+
                     for tid in self._hbg.tids:
                         n1 = last_read_nodes_current.setdefault(n.tid, {}).get(tid)
                         n2 = last_read_nodes_storage[parent].get(tid)
                         if n2 and ((not n1) or self._hbg.is_before_in_thread(n1, n2)):
                             last_read_nodes_current[n.tid][tid] = n2
-                
+
                 last_read_nodes_storage[n] = last_read_nodes_current.get(n.tid, {}).copy()
-                            
+
         return delta_ha_groups
-    
+
     def build_delta_ha_groups_opt2(self):
         ReadNode = nodes.InstructionNode
         WriteNode = nodes.InstructionNode
@@ -143,45 +143,45 @@ class PersistencyRaceDetector:
         found_writes_prev: Dict[ThreadId, NodeContextType] = {}
         found_writes_current: Dict[ThreadId, NodeContextType] = {}
         delta_ha_groups: Dict[ReadNode, Dict[Var, Set[WriteNode]]] = {}
-        
+
         def copy(d1: Dict[Any, Set[Any]]):
             d = {}
             for k, v in d1.items():
                 d[k] = v.copy()
             return d
-        
+
         def merge(d1: Dict[Any, Set[Any]], d2: Dict[Any, Set[Any]]):
             for k, v in d2.items():
                 d1.setdefault(k, set()).update(v)
-                
+
         def diff(d1: Dict[Any, Set[Any]], d2: Dict[Any, Set[Any]]) -> Dict[Any, Set[Any]]:
             d = {}
             for k, v in d1.items():
                 d[k] = v - d2.get(k, set())
             return d
-        
+
         for n in self._hbg.postorder():
             found_writes_current.setdefault(n.tid, {})
-            
+
             if n.itype == nodes.NodeType.WRITE:
                 n: WriteNode
                 found_writes_current[n.tid].setdefault(n.interval, set()).add(n)
-            
+
             elif n.itype == nodes.NodeType.READ:
                 n: ReadNode
-                
+
                 delta_ha_groups[n] = diff(found_writes_current[n.tid], found_writes_prev.get(n.tid, {}))
                 found_writes_prev[n.tid] = copy(found_writes_current[n.tid])
-                
+
             elif n.itype == nodes.NodeType.EPOCH:
                 n: nodes.EpochNode
                 for child in self._hbg.get_inter_children(n):
                     merge(found_writes_current[n.tid], found_writes_storage[child])
-                    
+
                 found_writes_storage[n] = copy(found_writes_current[n.tid])
-        
+
         return delta_ha_groups
-    
+
     def build_ha_groups_opt3(self):
         ReadNode = nodes.InstructionNode
         WriteNode = nodes.InstructionNode
@@ -189,89 +189,89 @@ class PersistencyRaceDetector:
         found_writes_storage: Dict[nodes.EpochNode, NodeContextType] = {}
         found_writes_current: Dict[ThreadId, NodeContextType] = {}
         ha_groups: Dict[ReadNode, Dict[Var, Set[WriteNode]]] = {}
-        
+
         def copy(d1: Dict[Any, Set[Any]]):
             d = {}
             for k, v in d1.items():
                 d[k] = v.copy()
             return d
-        
+
         def merge(d1: Dict[Any, Set[Any]], d2: Dict[Any, Set[Any]]):
             for k, v in d2.items():
                 d1.setdefault(k, set()).update(v)
-        
+
         for n in self._hbg.postorder():
             found_writes_current.setdefault(n.tid, {})
-            
+
             if n.itype == nodes.NodeType.WRITE:
                 n: WriteNode
                 found_writes_current[n.tid].setdefault(n.interval, set()).add(n)
-            
+
             elif n.itype == nodes.NodeType.READ:
                 n: ReadNode
                 ha_groups[n] = copy(found_writes_current[n.tid])
-                
+
             elif n.itype == nodes.NodeType.EPOCH:
                 n: nodes.EpochNode
                 for child in self._hbg.get_inter_children(n):
                     merge(found_writes_current[n.tid], found_writes_storage[child])
-                    
+
                 found_writes_storage[n] = copy(found_writes_current[n.tid])
-        
+
         return ha_groups
-    
+
     def build_ha_groups_opt4(self):
         ReadNode = nodes.InstructionNode
         WriteNode = nodes.InstructionNode
         ha_groups: Dict[ReadNode, Dict[Var, Set[WriteNode]]] = {}
-        
+
         for r in self._hbg.read_nodes:
             ha_groups[r] = {}
             for n in nx.dfs_postorder_nodes(self._hbg._graph, r):
                 if n.itype != nodes.NodeType.WRITE:
                     continue
-                
+
                 ha_groups[r].setdefault(n.interval, set()).add(n)
-        
+
         return ha_groups
-                
-    
+
+
     def build_delta_fw_groups_opt1(self):
         pass
-    
+
     def build_delta_fw_groups_opt2(self):
         pass
 
     def part_1(self):
         """Find delta groups"""
         thread_contexts = self._thread_contexts
-        
+
         for t in self._hbg.tids:
             thread_contexts.setdefault(t, ThreadContext())
             for _t in self._hbg.tids:
                 thread_contexts[t].lpw.setdefault(_t, {})
-        
+
         for n in nx.dfs_postorder_nodes(self._hbg):
             n: nodes.AbstractNode
             tid = n.tid
-            
+
             if isinstance(n, nodes.InstructionNode):
                 if n.itype == nodes.NodeType.WRITE:
                     thread_contexts[tid].lfw[tid] = n
-        
+
                 var = n.interval
-                
+
                 for t in self._hbg.tids:
                     if n.itype == nodes.NodeType.WRITE:
                         node = thread_contexts[tid].lpw[t].get(var)
                         if node:
                             self._delta_flushed_write_nodes.setdefault(node, {}).setdefault(var, set()).add(n)
-                    
+
                     elif n.itype == nodes.NodeType.READ:
                         node = thread_contexts[tid].lpw[t].get(var)
                         if node:
                             self._delta_flushed_read_nodes.setdefault(node, {}).setdefault(var, set()).add(n)
-                    
+
                     elif n.itype == nodes.NodeType.FLUSH:
                         if not thread_contexts[tid].lfw.get(t):
                             continue
@@ -284,20 +284,20 @@ class PersistencyRaceDetector:
                         return False
                     assert n1.tid == n2.tid
                     return self._hbg.get_node_location(n1).tindex < self._hbg.get_node_location(n2).tindex
-                
+
                 for c in self._hbg.get_inter_children(n):
                     for t in self._hbg.tids:
                         n1 = thread_contexts[c.tid].lfw.get(t)
                         n2 = thread_contexts[tid].lfw.get(t)
                         if is_before(n1, n2):
                             thread_contexts[tid].lfw[t] = thread_contexts[c.tid].lfw.get(t)
-                            
+
                         for var in thread_contexts[c.tid].lpw[t].keys():
                             n1 = thread_contexts[c.tid].lpw[t].get(var)
                             n2 = thread_contexts[tid].lpw[t].get(var)
                             if is_before(n1, n2):
                                 thread_contexts[tid].lpw[t][var] = thread_contexts[c.tid].lpw[t].get(var)
-    
+
     # def run(self):
     #     self._first_iteration()
     #     self._second_iteration()
