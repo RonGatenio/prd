3ca186aba0f8e3d7cb3cf3cc86e9dfbdf822e444
Ron Gatenio <ron.gatenio@gmail.com>
2022-10-12 01:27:25 +0300

    updated pdg to be more like hbg

--- pyprd/src/pdg.py
+++ pyprd/src/pdg.py
@@ -1,29 +1,33 @@
 import networkx as nx
-from typing import Dict, List, Set, Tuple
-from collections import namedtuple
-from intervaltree import IntervalTree, Interval
+from typing import Iterable
 import nodes
 from hbg import HBG
 
 
-class PDG(nx.DiGraph):
-    def __post_init__(self):
-        return self
+class PDG:
+    def __init__(self, base_graph: nx.DiGraph):
+        self._graph = base_graph
+
+    def get_dependencies(self, write_node: nodes.InstructionNode) -> Iterable[nodes.InstructionNode]:
+        return self._graph.neighbors(write_node)
+
+    def get_dependants(self, read_node: nodes.InstructionNode) -> Iterable[nodes.InstructionNode]:
+        return self._graph.predecessors(read_node)
 
 
 class PDGBuilder:
     def __init__(self):
-        self._pdg = PDG()
-        
+        self._graph = nx.DiGraph()
+
     def add_dependency(self, write_node: nodes.InstructionNode, read_node: nodes.InstructionNode):
         assert write_node.itype == nodes.NodeType.WRITE, f'expected a write node but got {write_node.itype}'
         assert read_node.itype == nodes.NodeType.READ, f'expected a read node but got {read_node.itype}'
         assert write_node.tid == read_node.tid, 'expected tids to be the same'
-        
-        self._pdg.add_edge(write_node, read_node)
-        
+
+        self._graph.add_edge(write_node, read_node)
+
     def build(self):
-        return self._pdg.__post_init__()
+        return PDG(self._graph)
 
 
 def generate_mock_pdg(hbg: HBG, threshold=10):
@@ -32,18 +36,18 @@ def generate_mock_pdg(hbg: HBG, threshold=10):
     This is a mock!
     """
     pdgbuilder = PDGBuilder()
-    
+
     for tid in hbg.tids:
         read_instruction_nodes = []
-        
+
         for n in hbg.get_thread_nodes(tid):
             n: nodes.AbstractNode
-            
+
             if n.itype == nodes.NodeType.READ:
                 read_instruction_nodes.append(n)
-                
+
             elif n.itype == nodes.NodeType.WRITE:
                 for read_node in read_instruction_nodes[-threshold:]:
                     pdgbuilder.add_dependency(n, read_node)
-    
+
     return pdgbuilder.build()
