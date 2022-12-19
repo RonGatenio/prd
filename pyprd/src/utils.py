51393f808a2813be39297eeb9dc9a9fb60c8155b
Ron Gatenio <ron.gatenio@gmail.com>
2022-12-20 00:11:18 +0200

    added a timeit util

--- pyprd/src/utils.py
+++ pyprd/src/utils.py
@@ -1,7 +1,16 @@
+import time
+from contextlib import contextmanager
 from config import DEFAULT_CACHELINE_SIZE
 
 
 def get_cacheline_address(address, cacheline_size=DEFAULT_CACHELINE_SIZE):
     mask = ((1 << 64) - 1) * cacheline_size
     return address & mask
-    
+
+
+@contextmanager
+def timeit(name):
+    s = time.time()
+    yield
+    total = time.time() - s
+    print(f'[*] {name:60} {total:.3f} sec')
