396ab436f6df013153563c7880e798154bcbb145
Ron Gatenio <ron.gatenio@gmail.com>
2024-01-18 15:36:50 +0200

    removed all unused sanitizers in compiler-rt from the cmake files

--- src/runtime/compiler-rt/cmake/config-ix.cmake
+++ src/runtime/compiler-rt/cmake/config-ix.cmake
@@ -615,7 +615,8 @@ if(COMPILER_RT_SUPPORTED_ARCH)
 endif()
 message(STATUS "Compiler-RT supported architectures: ${COMPILER_RT_SUPPORTED_ARCH}")
 
-set(ALL_SANITIZERS asan;dfsan;msan;hwasan;tsan;safestack;cfi;scudo;ubsan_minimal;gwp_asan)
+# set(ALL_SANITIZERS asan;dfsan;msan;hwasan;tsan;safestack;cfi;scudo;ubsan_minimal;gwp_asan)
+set(ALL_SANITIZERS )
 set(COMPILER_RT_SANITIZERS_TO_BUILD all CACHE STRING
     "sanitizers to build if supported on the target (all;${ALL_SANITIZERS})")
 list_replace(COMPILER_RT_SANITIZERS_TO_BUILD all "${ALL_SANITIZERS}")
