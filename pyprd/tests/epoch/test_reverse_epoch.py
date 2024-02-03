6ee43b3ff1fa14ad6cd9827ab08b8f0cebef624f
Ron Gatenio <ron.gatenio@gmail.com>
2024-02-04 00:53:33 +0200

    bug fix in test

--- pyprd/tests/epoch/test_reverse_epoch.py
+++ pyprd/tests/epoch/test_reverse_epoch.py
@@ -1,55 +1,55 @@
-from epoch import ReverseEpoch
+from epoch import ReversedEpoch
 
 
 def test_compare():
-    assert not (ReverseEpoch(0) == ReverseEpoch(1))
-    assert     (ReverseEpoch(0) != ReverseEpoch(1))
-    assert not (ReverseEpoch(0) <  ReverseEpoch(1))
-    assert not (ReverseEpoch(0) <= ReverseEpoch(1))
-    assert     (ReverseEpoch(0) >  ReverseEpoch(1))
-    assert     (ReverseEpoch(0) >= ReverseEpoch(1))
+    assert not (ReversedEpoch(0) == ReversedEpoch(1))
+    assert     (ReversedEpoch(0) != ReversedEpoch(1))
+    assert not (ReversedEpoch(0) <  ReversedEpoch(1))
+    assert not (ReversedEpoch(0) <= ReversedEpoch(1))
+    assert     (ReversedEpoch(0) >  ReversedEpoch(1))
+    assert     (ReversedEpoch(0) >= ReversedEpoch(1))
 
 
 def test_compare_same():
-    assert     (ReverseEpoch(0) == ReverseEpoch(0))
-    assert not (ReverseEpoch(0) != ReverseEpoch(0))
-    assert not (ReverseEpoch(0) <  ReverseEpoch(0))
-    assert     (ReverseEpoch(0) <= ReverseEpoch(0))
-    assert not (ReverseEpoch(0) >  ReverseEpoch(0))
-    assert     (ReverseEpoch(0) >= ReverseEpoch(0))
+    assert     (ReversedEpoch(0) == ReversedEpoch(0))
+    assert not (ReversedEpoch(0) != ReversedEpoch(0))
+    assert not (ReversedEpoch(0) <  ReversedEpoch(0))
+    assert     (ReversedEpoch(0) <= ReversedEpoch(0))
+    assert not (ReversedEpoch(0) >  ReversedEpoch(0))
+    assert     (ReversedEpoch(0) >= ReversedEpoch(0))
 
 
 def test_compare_min():
-    assert not (ReverseEpoch(0) == ReverseEpoch())
-    assert     (ReverseEpoch(0) != ReverseEpoch())
-    assert     (ReverseEpoch(0) <  ReverseEpoch())
-    assert     (ReverseEpoch(0) <= ReverseEpoch())
-    assert not (ReverseEpoch(0) >  ReverseEpoch())
-    assert not (ReverseEpoch(0) >= ReverseEpoch())
+    assert not (ReversedEpoch(0) == ReversedEpoch())
+    assert     (ReversedEpoch(0) != ReversedEpoch())
+    assert not (ReversedEpoch(0) <  ReversedEpoch())
+    assert not (ReversedEpoch(0) <= ReversedEpoch())
+    assert     (ReversedEpoch(0) >  ReversedEpoch())
+    assert     (ReversedEpoch(0) >= ReversedEpoch())
 
 
 def test_compare_none():
-    assert not (ReverseEpoch(0) == None)
-    assert     (ReverseEpoch(0) != None)
-    assert     (ReverseEpoch(0) <  None)
-    assert     (ReverseEpoch(0) <= None)
-    assert not (ReverseEpoch(0) >  None)
-    assert not (ReverseEpoch(0) >= None)
+    assert not (ReversedEpoch(0) == None)
+    assert     (ReversedEpoch(0) != None)
+    assert not (ReversedEpoch(0) <  None)
+    assert not (ReversedEpoch(0) <= None)
+    assert     (ReversedEpoch(0) >  None)
+    assert     (ReversedEpoch(0) >= None)
 
 
 def test_compare_min_left():
-    assert not (ReverseEpoch() == ReverseEpoch(0))
-    assert     (ReverseEpoch() != ReverseEpoch(0))
-    assert not (ReverseEpoch() <  ReverseEpoch(0))
-    assert not (ReverseEpoch() <= ReverseEpoch(0))
-    assert     (ReverseEpoch() >  ReverseEpoch(0))
-    assert     (ReverseEpoch() >= ReverseEpoch(0))
+    assert not (ReversedEpoch() == ReversedEpoch(0))
+    assert     (ReversedEpoch() != ReversedEpoch(0))
+    assert     (ReversedEpoch() <  ReversedEpoch(0))
+    assert     (ReversedEpoch() <= ReversedEpoch(0))
+    assert not (ReversedEpoch() >  ReversedEpoch(0))
+    assert not (ReversedEpoch() >= ReversedEpoch(0))
 
 
 def test_compare_none_left():
-    assert not (None == ReverseEpoch(0))
-    assert     (None != ReverseEpoch(0))
-    assert not (None <  ReverseEpoch(0))
-    assert not (None <= ReverseEpoch(0))
-    assert     (None >  ReverseEpoch(0))
-    assert     (None >= ReverseEpoch(0))
+    assert not (None == ReversedEpoch(0))
+    assert     (None != ReversedEpoch(0))
+    assert     (None <  ReversedEpoch(0))
+    assert     (None <= ReversedEpoch(0))
+    assert not (None >  ReversedEpoch(0))
+    assert not (None >= ReversedEpoch(0))
