4274a68ba04b54a34c67dbcc1bc8ace2e9904b20
Ron Gatenio <ron.gatenio@gmail.com>
2024-02-04 00:54:48 +0200

    added DefaultDictByKey class

--- pyprd/src/utils.py
+++ pyprd/src/utils.py
@@ -1,5 +1,7 @@
+import collections
 import time
 from contextlib import contextmanager
+from typing import Callable, TypeVar
 from config import DEFAULT_CACHELINE_SIZE
 
 
@@ -14,3 +16,19 @@ def timeit(name):
     yield
     total = time.time() - s
     print(f'[*] {name:60} {total:.3f} sec')
+
+K = TypeVar("K")
+V = TypeVar("V")
+class DefaultDictByKey(collections.defaultdict):
+    def __init__(self, default_factory: Callable[[K], V]=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not callable(default_factory) and default_factory is not None:
+            raise TypeError('first argument must be callable or None')
+        self.default_factory = default_factory
+
+    def __missing__(self, key: K) -> V:
+        if self.default_factory is None:
+            raise KeyError(key)
+        if key not in self:
+            self[key] = self.default_factory(key)
+        return self[key]
