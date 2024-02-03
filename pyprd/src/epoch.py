c665a2965d949cf184e8f268803798089f98b585
Ron Gatenio <ron.gatenio@gmail.com>
2024-02-03 20:06:19 +0200

    modified epoch classes

--- pyprd/src/epoch.py
+++ pyprd/src/epoch.py
@@ -1,10 +1,9 @@
 
-class Epoch(int):
+class EpochBase(int):
     MIN = -1
     
     def __new__(cls, x=None):
-        if x is None or x == cls.MIN:
-            x = cls.MIN
+        x = cls.MIN if x is None or x == cls.MIN else x
         return super().__new__(cls, x)
     
     def __eq__(self, other: 'Epoch') -> bool:
@@ -13,41 +12,30 @@ class Epoch(int):
     def __ne__(self, other: 'Epoch') -> bool:
         return super().__ne__(self.__class__(other))
 
-    def __gt__(self, other: 'Epoch') -> bool:
+
+class Epoch(EpochBase):
+    def __gt__(self, other: 'EpochBase') -> bool:
         return super().__gt__(self.__class__(other))
     
-    def __ge__(self, other: 'Epoch') -> bool:
+    def __ge__(self, other: 'EpochBase') -> bool:
         return super().__ge__(self.__class__(other))
     
-    def __lt__(self, other: 'Epoch') -> bool:
+    def __lt__(self, other: 'EpochBase') -> bool:
         return super().__lt__(self.__class__(other))
     
-    def __le__(self, other: 'Epoch') -> bool:
+    def __le__(self, other: 'EpochBase') -> bool:
         return super().__le__(self.__class__(other))
 
 
-class ReverseEpoch(int):
-    MIN = -1
-
-    def __new__(cls, x=None):
-        if x is None or x == cls.MIN:
-            x = cls.MIN
-        return super().__new__(cls, x)
-    
-    def __eq__(self, other: 'Epoch') -> bool:
-        return super().__eq__(self.__class__(other))
-    
-    def __ne__(self, other: 'Epoch') -> bool:
-        return super().__ne__(self.__class__(other))
-
-    def __gt__(self, other: 'Epoch') -> bool:
+class ReverseEpoch(EpochBase):
+    def __gt__(self, other: 'EpochBase') -> bool:
         return super().__lt__(self.__class__(other))
     
-    def __ge__(self, other: 'Epoch') -> bool:
+    def __ge__(self, other: 'EpochBase') -> bool:
         return super().__le__(self.__class__(other))
     
-    def __lt__(self, other: 'Epoch') -> bool:
+    def __lt__(self, other: 'EpochBase') -> bool:
         return super().__gt__(self.__class__(other))
     
-    def __le__(self, other: 'Epoch') -> bool:
+    def __le__(self, other: 'EpochBase') -> bool:
         return super().__ge__(self.__class__(other))
