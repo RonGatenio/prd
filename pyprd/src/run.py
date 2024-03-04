418b834fd1841eadb4faa0dc504930e9fb004839
Ron Gatenio <ron.gatenio@gmail.com>
2024-03-04 03:23:53 +0200

    updated run script

--- pyprd/src/run.py
+++ pyprd/src/run.py
@@ -11,33 +11,24 @@ def main():
 
     args = parser.parse_args()
 
-
     trace = TraceParser.from_file(args.trace)
-    # with timeit('hbg without dc'):
-    #     hbg = trace.to_hbg(make_daisy_chains=False)
 
-    # with timeit('trace file parsing'):
-    #     trace = TraceParser.from_file(r'C:\Home\Projects\llvm-project\pyprd\real_tests\2022-12-16\trace.txt')
-    with timeit('hbg with dc'):
-        # hbg = trace.to_hbg(max_lines=1000000)
+    with timeit('hbg build'):
         hbg = trace.to_hbg(filter_volatile_nodes=True)
 
     with timeit('hbg stats'):
         print(hbg.stats())
     
-    with timeit('pdg'):
+    with timeit('pdg build'):
         pdg = generate_mock_pdg_v2(hbg)
 
     prd = PersistencyRaceDetector(hbg, pdg)
 
     with timeit('find races'):
-        races = prd.run(show_first_bug_only=True)
-        # for race in prd.run(show_first_bug_only=True):
-        #     print(race)
+        prd.run(show_first_bug_only=True)
 
     print(' --- ')
     print(prd.races)
-    return
 
 
 if __name__ == "__main__":
