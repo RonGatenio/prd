363bf1d0035bb02d68d35cc8a1c47e843c584782
Ron Gatenio <ron.gatenio@gmail.com>
2024-03-08 20:00:56 +0200

    updated run.py

--- pyprd/src/run.py
+++ pyprd/src/run.py
@@ -5,30 +5,91 @@ from utils import timeit
 import argparse
 
 
+def run(hbg, pdg,
+        ignore_inter_thread_edges=False,
+        ignore_flush_nodes=False,
+        ignore_persisted_before_index=False,
+        ignore_happens_after_index=False,
+        ignore_read_node_persistency=False,
+        show_only_first_bug_in_thread=False):
+    prd = PersistencyRaceDetector(hbg, pdg,
+                                  ignore_inter_thread_edges     = ignore_inter_thread_edges,
+                                  ignore_flush_nodes            = ignore_flush_nodes,
+                                  ignore_persisted_before_index = ignore_persisted_before_index,
+                                  ignore_happens_after_index    = ignore_happens_after_index,
+                                  ignore_read_node_persistency  = ignore_read_node_persistency,
+                                  show_only_first_bug_in_thread = show_only_first_bug_in_thread)
+    prd.run()
+    print(prd.stats())
+    return prd
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("trace")
 
     args = parser.parse_args()
 
-    trace = TraceParser.from_file(args.trace)
+    with timeit('open trace file'):
+        trace = TraceParser.from_file(args.trace)
 
     with timeit('hbg build'):
         hbg = trace.to_hbg(filter_volatile_nodes=True)
 
     with timeit('hbg stats'):
-        print(hbg.stats())
-    
+        print(hbg.stats(True))
+
     with timeit('pdg build'):
         pdg = generate_mock_pdg_v2(hbg)
 
-    prd = PersistencyRaceDetector(hbg, pdg)
+    prd = run(hbg, pdg,
+        ignore_inter_thread_edges=False,
+        ignore_flush_nodes=False,
+        ignore_persisted_before_index=False,
+        ignore_happens_after_index=False,
+        ignore_read_node_persistency=False,
+        show_only_first_bug_in_thread=False)
 
-    with timeit('find races'):
-        prd.run(show_first_bug_only=True)
+    # run(hbg, pdg,
+    #     ignore_inter_thread_edges=False,
+    #     ignore_flush_nodes=False,
+    #     ignore_persisted_before_index=True,
+    #     ignore_happens_after_index=False,
+    #     ignore_read_node_persistency=False,
+    #     show_only_first_bug_in_thread=False)
 
-    print(' --- ')
-    print(prd.races)
+    # run(hbg, pdg,
+    #     ignore_inter_thread_edges=False,
+    #     ignore_flush_nodes=True,
+    #     ignore_persisted_before_index=False,
+    #     ignore_happens_after_index=False,
+    #     ignore_read_node_persistency=False,
+    #     show_only_first_bug_in_thread=False)
+    
+    # run(hbg, pdg,
+    #     ignore_inter_thread_edges=False,
+    #     ignore_flush_nodes=False,
+    #     ignore_persisted_before_index=True,
+    #     ignore_happens_after_index=True,
+    #     ignore_read_node_persistency=True,
+    #     show_only_first_bug_in_thread=False)
+    
+    # run(hbg, pdg,
+    #     ignore_inter_thread_edges=True,
+    #     ignore_flush_nodes=True,
+    #     ignore_persisted_before_index=True,
+    #     ignore_happens_after_index=True,
+    #     ignore_read_node_persistency=True,
+    #     show_only_first_bug_in_thread=False)
+
+    print('')
+    
+    tops = [
+        "main test.cpp",
+        "main::$_1::operator()(int, int) const test.cpp:",
+    ]
+    
+    print(prd.races.to_str(callstack_top=tops))
 
 
 if __name__ == "__main__":
