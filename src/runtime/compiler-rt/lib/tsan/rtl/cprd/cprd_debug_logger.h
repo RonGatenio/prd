086a4bd46eec0ba432ebd9dc45b42d716204f5ae
Ron Gatenio <ron.gatenio@gmail.com>
2024-03-14 03:35:50 +0200

    adding only one \n in log prints

--- src/runtime/compiler-rt/lib/tsan/rtl/cprd/cprd_debug_logger.h
+++ src/runtime/compiler-rt/lib/tsan/rtl/cprd/cprd_debug_logger.h
@@ -1,8 +1,17 @@
 #ifndef CPRD_DEBUG_LOGGER_H
 #define CPRD_DEBUG_LOGGER_H
 
+
+
 #ifdef CPRD_DEBUG
-#define _LOG(level, fmt, ...) Printf("[CPRD] [%s] [%d %s %s] " fmt "\n", level, cur_thread()->tid, __func__, __FILE__, __VA_ARGS__)
+
+template<unsigned N> constexpr
+const char* _new_line(const char (&s) [N]) {
+    return s[N-2] == '\n' ? "" : "\n";
+}
+
+// #define _LOG(level, fmt, ...) Printf("[CPRD] [%s] [%d %s %s] " fmt "%s", level, cur_thread()->tid, __func__, __FILE__, __VA_ARGS__, _new_line(fmt))
+#define _LOG(level, fmt, ...) Printf("[CPRD] [%s] [%d %s] " fmt "%s", level, cur_thread()->tid, __func__, __VA_ARGS__, _new_line(fmt))
 #else
 #define _LOG(fmt, ...)
 #endif
