e01549c727ec427fe4682b7c637cb07e09fe783b
Ron Gatenio <ron.gatenio@gmail.com>
2024-03-19 05:02:09 +0200

    pyprd - typing

--- pyprd/src/pycprd/utils.py
+++ pyprd/src/pycprd/utils.py
@@ -5,12 +5,12 @@ from contextlib import contextmanager
 from .config import DEFAULT_CACHELINE_SIZE
 
 
-def get_cacheline_address(address, cacheline_size=DEFAULT_CACHELINE_SIZE):
+def get_cacheline_address(address, cacheline_size=DEFAULT_CACHELINE_SIZE) -> int:
     mask = ((1 << 64) - 1) * cacheline_size
     return address & mask
 
 
-def get_cacheline_interval(address, size, cacheline_size=DEFAULT_CACHELINE_SIZE):
+def get_cacheline_interval(address, size, cacheline_size=DEFAULT_CACHELINE_SIZE) -> tuple[int, int]:
     mask = ((1 << 64) - 1) * cacheline_size
     start = address & mask
     end = (address + size + cacheline_size - 1) & mask
