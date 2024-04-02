07b2e71ce24a7f25489e75539f1d6a13836ff86d
Ron Gatenio <ron.gatenio@gmail.com>
2024-04-02 13:19:31 +0300

    bug fix in PDG - added dependencies of same cachline vars before

--- pyprd/src/pycprd/pdg.py
+++ pyprd/src/pycprd/pdg.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 from statistics import mean
 import networkx as nx
 from typing import Iterable
@@ -25,11 +26,10 @@ class PDGBuilder:
     def __init__(self):
         self._graph = nx.DiGraph()
         self._dependencies: dict[int, set[int]] = defaultdict(set)
-        self._total_dependencies = 0
         
     @property
     def total_dependencies(self):
-        return self._total_dependencies
+        return len(self._graph) + sum(map(len, self._dependencies.values()))
 
     def add_dependency(self, write_node: nodes.InstructionNode, read_node: nodes.InstructionNode):
         assert write_node.itype == nodes.NodeType.WRITE, f'expected a write node but got {write_node.itype}'
@@ -37,18 +37,16 @@ class PDGBuilder:
         assert write_node.tid == read_node.tid, 'expected tids to be the same'
 
         self._graph.add_edge(write_node, read_node)
-        self._total_dependencies += 1
         
     def add_dependency_pc(self, write_node_pc: int, read_node_pc: int):
         self._dependencies[write_node_pc].add(read_node_pc)
-        self._total_dependencies += 1
 
     def build(self, hbg: HBG = None):
         if self._dependencies:
             assert hbg, 'There are PC based dependencies, HBG is required'
 
             for tid in hbg.tids:
-                pending_reads = defaultdict(set)
+                pending_reads: dict[int, set[nodes.ReadNode]] = defaultdict(set)
                 
                 for n in hbg.get_thread_nodes(tid):
                     n: nodes.AbstractNode
@@ -60,8 +58,12 @@ class PDGBuilder:
                     elif n.itype == nodes.NodeType.WRITE:
                         n: nodes.WriteNode
                         for read_pc in self._dependencies[n.pc]:
+                            pending_reads_to_remove = set()
                             for read_node in pending_reads[read_pc]:
-                                self.add_dependency(n, read_node)
+                                if n.get_cacheline_interval() != read_node.get_cacheline_interval():
+                                    self.add_dependency(n, read_node)
+                                    pending_reads_to_remove.add(read_node)
+                            pending_reads[read_pc] -= pending_reads_to_remove
             
         return PDG(self._graph)
 
@@ -110,7 +112,7 @@ def generate_mock_pdg_v2(hbg: HBG) -> PDG:
     return pdgbuilder.build()
 
 
-def pdg_distance(hbg: HBG, pdg1: PDG, pdg2: PDG) -> tuple[int, dict[nodes.ReadNode, int]]:
+def pdg_distance(hbg: HBG, pdg1: PDG, pdg2: PDG) -> tuple[int, dict[nodes.ReadNode, int], int]:
     distance_per_read = {}
 
     for tid in hbg.tids:
@@ -119,11 +121,16 @@ def pdg_distance(hbg: HBG, pdg1: PDG, pdg2: PDG) -> tuple[int, dict[nodes.ReadNo
         
         read_nodes = (n for n in hbg.get_thread_nodes(tid) if n.itype == nodes.NodeType.READ)
         for r in read_nodes:
-            pdg1_first_dependant_write_index = min(write_locations[w] for w in pdg1.get_dependants(r))
-            pdg2_first_dependant_write_index = min(write_locations[w] for w in pdg2.get_dependants(r))
+            pdg1_first_dependant_write_index = min((write_locations[w] for w in pdg1.get_dependants(r)), default=math.inf)
+            pdg2_first_dependant_write_index = min((write_locations[w] for w in pdg2.get_dependants(r)), default=math.inf)
             
             _distance = pdg2_first_dependant_write_index - pdg1_first_dependant_write_index
             distance_per_read[r] = _distance
-                
-    distance = mean(distance_per_read.values())
-    return distance, distance_per_read
+    
+    distances = [v for v in distance_per_read.values() if math.isfinite(v)]
+    
+    assert all(v >= 0 for v in distances), "There are negative distances when comparing to mock -mock must be optimal"
+
+    mean_distance = mean(distances)
+    missing_dependants = len(list(v for v in distance_per_read.values() if math.isinf(v)))
+    return mean_distance, distance_per_read, missing_dependants
