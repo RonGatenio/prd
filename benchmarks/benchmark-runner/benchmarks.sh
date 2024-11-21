6eb480e392a3179f6394505fa7be132409706d76
Ron Gatenio <ron.gatenio@gmail.com>
2024-11-22 00:36:27 +0200

    added more running configs

--- benchmarks/benchmark-runner/benchmarks.sh
+++ benchmarks/benchmark-runner/benchmarks.sh
@@ -3,15 +3,24 @@
 python3 benchmark_runner.py FAST_FAIR --nkeys 10
 python3 benchmark_runner.py CCEH --nkeys 10
 python3 benchmark_runner.py RECIPE --nkeys 10
+python3 benchmark_runner.py MEMCACHED --nkeys 10
 
 python3 benchmark_runner.py FAST_FAIR --nkeys 100
 python3 benchmark_runner.py CCEH --nkeys 100
 python3 benchmark_runner.py RECIPE --nkeys 100
+python3 benchmark_runner.py MEMCACHED --nkeys 100
 
 python3 benchmark_runner.py FAST_FAIR --nkeys 1000
 python3 benchmark_runner.py CCEH --nkeys 1000
 python3 benchmark_runner.py RECIPE --nkeys 1000
+python3 benchmark_runner.py MEMCACHED --nkeys 1000
 
 python3 benchmark_runner.py FAST_FAIR --nkeys 10000
 python3 benchmark_runner.py CCEH --nkeys 10000
 python3 benchmark_runner.py RECIPE --nkeys 10000
+python3 benchmark_runner.py MEMCACHED --nkeys 10000
+
+python3 benchmark_runner.py FAST_FAIR --nkeys 100000
+python3 benchmark_runner.py CCEH --nkeys 100000
+python3 benchmark_runner.py RECIPE --nkeys 100000
+python3 benchmark_runner.py MEMCACHED --nkeys 100000
